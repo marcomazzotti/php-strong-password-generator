306135f7ee0d718896e37ee111ad3ad469d17bbf
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-05 16:45:48 +0200

    Add: Milestone 2

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
-$password = "";
-$lower_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-$upper_array = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-$number_array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-$symbol_array = ['@', '#', '/', '*', '?', '&', '$', '!', '^', '+', ':', ';', '<', '>', '%'];
-
-if (isset($_GET["password-length"])) {
-  $length = $_GET['password-length'];
-  $user_array = array_merge($lower_array, $upper_array, $number_array, $symbol_array);
-  for ($i = 0; $i < $length; $i++) {
-    $random_number = rand(0, (count($user_array) - 1));
-    $password = $password . $user_array[$random_number];
-  }
-}
+include_once __DIR__ . "/partials/functions.php";
+include_once __DIR__ . "/partials/data.php";
 ?>
 
 <!DOCTYPE html>
---
+++ partials/functions.php
@@ -0,0 +1,10 @@
+<?php
+include_once __DIR__ . "/data.php";
+if (isset($_GET["password-length"])) {
+  $length = $_GET['password-length'];
+  $user_array = array_merge($lower_array, $upper_array, $number_array, $symbol_array);
+  for ($i = 0; $i < $length; $i++) {
+    $random_number = rand(0, (count($user_array) - 1));
+    $password = $password . $user_array[$random_number];
+  }
+}
