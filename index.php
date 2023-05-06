0579615ccfd8ef4330ab51577c8abac5a095b034
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-06 14:42:07 +0200

    Add: style.css

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ include_once __DIR__ . "/partials/data.php";
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="style/style.css">
   <title>Strong Password Generator</title>
 </head>
 
@@ -18,12 +19,14 @@ include_once __DIR__ . "/partials/data.php";
   <div class="container text-center">
     <h1>Strong Password Generator</h1>
     <h2>Genera una password sicura</h2>
-    <form action="index.php" method="get">
-      <label for="password-length">Lunghezza password</label>
-      <input type="number" name="password-length" id="password-length">
-      <button type="submit" class="btn btn-success">Genera</button>
-    </form>
-
+    <div class="wrapper">
+      <form action="index.php" method="get">
+        <label for="password-length">Lunghezza password?</label>
+        <br>
+        <input type="number" name="password-length" id="password-length">
+        <button type="submit">Genera</button>
+      </form>
+    </div>
   </div>
 </body>
 
--- password.php
+++ password.php
@@ -9,11 +9,20 @@ session_start();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="style/style.css">
   <title>Your Password</title>
 </head>
 
 <body>
-  <h3>La tua password: <?php echo $_SESSION["password"] ?></h3>
+  <div class="container text-center">
+    <h1>Strong Password Generator</h1>
+    <h2>Genera una password sicura</h2>
+    <div class="wrapper">
+      <h5>La tua password:</h5>
+      <h6><?php echo $_SESSION["password"] ?></h6>
+    </div>
+  </div>
 </body>
 
 </html>
